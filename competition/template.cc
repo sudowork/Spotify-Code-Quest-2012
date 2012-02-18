fcd75ddf6d97d2b5fdfad8868ab827b0f9fa7f59
Kevin Gao <kevin.gao@duke.edu>
2012-02-18 13:47:48 -0500

    Only committing sources

---
+++ competition/template.cc
@@ -0,0 +1,21 @@
+/* Spotify Code Quest 2012
+ * @author Kevin Gao
+ */
+
+#include <iostream>
+
+using namespace std;		// Because I'm lazy
+
+int main() {
+	// Make io faster
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	// Main
+	int a,b;
+	while (cin >> a >> b) {
+	}
+	
+	
+	return 0;
+}
